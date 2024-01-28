933b3de0ecb802efd3ba3c6117a679a3922c504e
HARSH SHUKLA <HARSHSHUKLA2022@VITBHOPAL.AC.IN>
2024-01-28 20:07:25 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import React, { useState } from "react";
-import Login from "./Login";
-import Home from "./Home";
+import successGif from "./no.gif";
+import failureGif from "./yes.gif";
+import successSound from "./success.mp3";
+import failureSound from "./notsuccess.mp3";
 import "./App.css";
 
-const App = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
+const correctPassword = "Amit bhai";
 
-  const handleLogin = () => {
-    setLoggedIn(true);
+const App = () => {
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [gif, setGif] = useState(null);
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
-  const handleLogout = () => {
-    setLoggedIn(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === correctPassword) {
+      setMessage("Success! You entered the correct password.");
+      setGif(successGif);
+      playSound(successSound);
+    } else {
+      setMessage("Oops! Incorrect password. Please try again.");
+      setGif(failureGif);
+      playSound(failureSound);
+    }
+  };
+
+  const playSound = (audio) => {
+    const sound = new Audio(audio);
+    sound.play();
   };
 
   return (
-    <div className={`app-container ${loggedIn ? "logged-in" : ""}`}>
-      <video autoPlay muted loop className="background-video">
-        <source src="/hacking-vdeo.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      {loggedIn ? (
-        <Home onLogout={handleLogout} />
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
+    <div className="App">
+      <div className="container">
+        <h1>Password Validation</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Enter password..."
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <p>{message}</p>
+        {gif && <img src={gif} alt="Gif" />}
+      </div>
     </div>
   );
 };
-
 export default App;
